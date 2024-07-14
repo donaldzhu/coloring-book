bc4b231e681dff8ed9448587c604da753331c336
donaldzhu <donaldzhu.toronto@gmail.com>
2024-07-14 00:34:46 -0400

    added transform methods to Rect; added partial drawing

--- src/grid.js
+++ src/grid.js
@@ -1,17 +1,20 @@
 import { WIDTH_INCH, MIN_INCREMENT_INCH, HEIGHT_INCH, MAX_DIV, MIN_DIV, MARGIN } from './data/config'
-import { ASPECT_RATIO, INNER_ASPECT_RATIO } from './data/constants'
 import Rect from './utils/rect'
-import { cover, quickArray, randInt, vh, vw } from './utils/utils'
+import { map, quickArray, randInt } from './utils/utils'
 
 class Grid extends Rect {
   constructor() {
-    const { w, h } = cover(vw(), vh(), INNER_ASPECT_RATIO)
+    const baseColCount = Math.floor(WIDTH_INCH / MIN_INCREMENT_INCH)
+    const baseRowCount = Math.floor(HEIGHT_INCH / MIN_INCREMENT_INCH)
+
     super({
-      w,
-      h,
+      w: baseColCount,
+      h: baseRowCount
     })
     this.cols = []
     this.rows = []
+    this.baseColCount = baseColCount
+    this.baseRowCount = baseRowCount
   }
 
   create() {
@@ -26,16 +29,22 @@ class Grid extends Rect {
     w = this.w,
     h = this.h
   }) {
-    const dx = x - this.x
-    const dy = y - this.y
-    const wFactor = w / this.w
-    const hFactor = h / this.h
-    // need setters
+    const oldX1 = this.x1
+    const oldX2 = this.x2
+    const oldY1 = this.y1
+    const oldY2 = this.y2
+
     this.x = x
     this.y = y
     this.w = w
     this.h = h
-    const cols = this.cols = this.cols.map(col => col) // get transform matrix order
+
+    // console.log(this.x1, this.x2, this.y1, this.y2)
+    const cols = this.cols =
+      this.cols.map(col => map(col, oldX1, oldX2, this.x1, this.x2))
+    const rows = this.rows =
+      this.rows.map(row => map(row, oldY1, oldY2, this.y1, this.y2))
+    return { cols, rows }
   }
 
   getRandDiv(baseGridCount) {
@@ -47,20 +56,11 @@ class Grid extends Rect {
       return colIndex
     })
       .sort((a, b) => a - b)
-      .map(coor => coor * this.inchToPx(MIN_INCREMENT_INCH))
+    //.map(coor => coor * this.inchToPx(MIN_INCREMENT_INCH))
   }
 
   inchToPx(inch) {
     return inch * this.w / WIDTH_INCH
-  }
-
-
-  get baseColCount() {
-    return Math.floor(this.w / this.inchToPx(MIN_INCREMENT_INCH))
-  }
-
-  get baseRowCount() {
-    return Math.floor(this.h / this.inchToPx(MIN_INCREMENT_INCH))
   }
 
   get colW() {
--- src/index.js
+++ src/index.js
@@ -1,34 +1,33 @@
-import { MIN_DIV, SCREEN_MARGIN } from './data/config'
+import { INNER_ASPECT_RATIO } from './data/constants'
 import Grid from './grid'
+import { cover, vh, vw } from './utils/utils'
 
 window.setup = () => {
   createCanvas(window.innerWidth, window.innerHeight)
   background(255)
   const grid = new Grid()
   grid.create()
+
+  const margin = 100
+  const { w, h } = cover(vw() - margin * 2, vh() - margin * 2, INNER_ASPECT_RATIO)
+  grid.transform({ x: margin, y: margin, w, h })
   console.log(grid)
 
   // const yBounds = [grid.y1, grid.y2]
   // const xBounds = [grid.x1, grid.x2]
 
-  // window.stroke(0, 0, 0)
-  // grid.rows.forEach(row => {
-  //   const y = row * grid.h / grid.baseRowCount + SCREEN_MARGIN
-  //   window.line(xBounds[0], y, xBounds[1], y)
-  // })
+  window.stroke(0, 0, 0)
+  grid.rows.forEach(row => {
+    window.line(grid.x1, row, grid.x2, row)
+  })
 
+  grid.cols.forEach(col => {
+    window.line(col, grid.y1, col, grid.y2)
+  })
 
-  // grid.cols.forEach(col => {
-  //   const x = col * grid.w / grid.baseColCount + SCREEN_MARGIN
-  //   window.line(x, yBounds[0], x, yBounds[1])
-  // })
-
-
-  // window.stroke(255, 0, 0)
-  // window.line(xBounds[0], yBounds[0], xBounds[1], yBounds[0])
-  // window.line(xBounds[0], yBounds[1], xBounds[1], yBounds[1])
-  // window.line(xBounds[0], yBounds[0], xBounds[0], yBounds[1])
-  // window.line(xBounds[1], yBounds[0], xBounds[1], yBounds[1])
+  window.stroke(255, 0, 0)
+  window.noFill()
+  window.rect(...grid.topLeft, ...grid.botRight)
 }
 
 window.draw = () => {
